cbba743e35eacab2a6ebc06ff92b0f9337c1bdea
Chitakov <chitakov2606nikita@gmail.com>
2022-06-12 23:50:53 +0300

    Add project files.

---
+++ Lab7_2/Polynomial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Polynomial
+    {
+        private readonly double[] _coefficients;
+
+        //  Создание полинома на основе коэффициентов
+        public Polynomial(params double[] coefficients)
+        {
+            _coefficients = coefficients;
+        }
+        //  Получение или установка значения коэффициента*полинома
+        public double this[int n]
+        {
+            get { return _coefficients[n]; }
+            set { _coefficients[n] = value; }
+        }
+        //  Степень полинома
+        public int Order
+        {
+            get { return _coefficients.Length; }
+        }
+
+        // расчет значения полинома по схеме Горнера.
+        public double Calculate(double x)
+        {
+            int n = _coefficients.Length - 1;
+            double result = _coefficients[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result = x * result + _coefficients[i];
+            }
+            return result;
+        }
+        //  Сложение полиномов
+        public static Polynomial operator +(Polynomial pFirst, Polynomial pSecond)
+        {
+            int itemsCount = Math.Max(pFirst._coefficients.Length, pSecond._coefficients.Length);
+            var result = new double[itemsCount];
+            for (int i = 0; i < itemsCount; i++)
+            {
+                double a = 0;
+                double b = 0;
+                if (i < pFirst._coefficients.Length)
+                {
+                    a = pFirst[i];
+                }
+                if (i < pSecond._coefficients.Length)
+                {
+                    b = pSecond[i];
+                }
+                result[i] = a + b;
+            }
+            return new Polynomial(result);
+        }
+        //  Вычитание полиномов
+        public static Polynomial operator -(Polynomial pFirst, Polynomial pSecond)
+        {
+            int itemsCount = Math.Max(pFirst._coefficients.Length, pSecond._coefficients.Length);
+            var result = new double[itemsCount];
+            for (int i = 0; i < itemsCount; i++)
+            {
+                double a = 0;
+                double b = 0;
+                if (i < pFirst._coefficients.Length)
+                {
+                    a = pFirst[i];
+                }
+                if (i < pSecond._coefficients.Length)
+                {
+                    b = pSecond[i];
+                }
+                result[i] = a - b;
+            }
+            return new Polynomial(result);
+        }
+        //  Умножение полиномов
+        public static Polynomial operator *(Polynomial pFirst, Polynomial pSecond)
+        {
+            int itemsCount = pFirst._coefficients.Length + pSecond._coefficients.Length - 1;
+            var result = new double[itemsCount];
+            for (int i = 0; i < pFirst._coefficients.Length; i++)
+            {
+                for (int j = 0; j < pSecond._coefficients.Length; j++)
+                {
+                    result[i + j] += pFirst[i] * pSecond[j];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+        
+        //  Вывод на экран многочлена
+        public void PrintPolynomial()
+        {
+            int temp = this._coefficients.Length;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this._coefficients.Length; i++)
+            {   if (this[i] != 0)
+                {
+                    if (i == 0)
+                    {
+                        sb.Append(this[i]);
+                    }
+                    else
+                    {
+                        if (this[i] > 0)
+                        {
+                            sb.Append("+" + this[i] + "*x^" + i);
+                        }
+                        else
+                        {
+                            sb.Append(this[i] + "*x^" + i);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ Lab7_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial p1 = new Polynomial(1, 2, 3);
+            Polynomial p2 = new Polynomial(10, 20, 0, 40);
+            Polynomial sum = new Polynomial();
+            Polynomial sub = new Polynomial();
+            Polynomial multi = new Polynomial();
+
+            Console.WriteLine("Многочлен 1:");
+            p1.PrintPolynomial();
+            Console.WriteLine("Многочлен 2:");
+            p2.PrintPolynomial();
+            Console.WriteLine();
+
+            sum = p1 + p2;
+            Console.WriteLine("Сумма:");
+            sum.PrintPolynomial();
+            sub = p1 - p2;
+            Console.WriteLine("Разность:");
+            sub.PrintPolynomial();
+            Console.WriteLine("Произведение:");
+            multi = p1 * p2;
+            multi.PrintPolynomial();
+            Console.WriteLine();
+
+            Console.WriteLine("Введите чему равен x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Результат многочлена 1 при x = " + x);
+            Console.WriteLine(p1.Calculate(x));
+            Console.WriteLine("Результат многочлена 2 при x = " + x);
+            Console.WriteLine(p2.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
